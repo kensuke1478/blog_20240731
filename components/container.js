59f8c1cb2556dbd5a0caa4ae8d31007bb54e6bb1
kensuke <miotyan_shade@yahoo.co.jp>
2024-07-24 17:45:16 +0900

    first commit

---
+++ components/container.js
@@ -0,0 +1,11 @@
+import styles from 'styles/container.module.css'
+
+export default function Container({ children,large = false }) {
+  return (
+    <div className={large ? styles.large : styles.default}>
+      {children}
+    </div>
+  )
+}
+
+
